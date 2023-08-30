c2a4170a7f5b9911a50da90be55ecc633e4a4372
luong.an <luong.an@outlook.com>
2023-08-30 07:33:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean weekday = true;
+        boolean vacation = false;
+
+        if (weekday || vacation == true)
+            System.out.println("James sleeps late");;
+    }
+}
